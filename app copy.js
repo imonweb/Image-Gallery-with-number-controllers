a496fb5c12fd600cef72173108245af07408e96a
imonweb <contact@imonweb.co.uk>
2021-07-06 14:56:58 +0100

    completed

--- app copy.js
+++ app copy.js
@@ -1,43 +1,25 @@
-const btn = document.querySelectorAll('.btn');
-const gallery = document.querySelector('#gallery');
+var btn = document.getElementsByClassName('btn');
+var gallery = document.getElementById('gallery');
+ 
 
-// let current = document.querySelector('.active');
-
-const images = [
+var images = [
   "images/pic1.jpg",
   "images/pic2.jpg",
   "images/pic3.jpg",
   "images/pic4.jpg"
 ];
 
-
-// btn.forEach(b => b.addEeventListener('click', imgClick));
-
-// function imgClick() {
-
-// }
-
-
+ 
 
 for(let i = 0; i < btn.length; i++){
  
-  btn[i].addEventListener('click', () => {
-    
-    gallery.src = images[i];
-    
-    let current = document.querySelector('.active');
-    current[0].className = current[0].className.replace('active', '');
-    this.className += ' active';
-  });
-
-  
-
-/*
   btn[i].onclick = function() {
     gallery.src = images[i];
-    let current = document.querySelector('.active');
+
+    let current = document.getElementsByClassName('active');
     current[0].className = current[0].className.replace('active', '');
     this.className += ' active';
   }
-*/
+
+ 
 }
--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-const btn = document.querySelectorAll('.btn');
-const gallery = document.querySelector('#gallery');
-
-// let current = document.querySelector('.active');
+const btn = document.getElementsByClassName('btn');
+const gallery = document.getElementById('gallery');
+ 
 
 const images = [
   "images/pic1.jpg",
@@ -10,34 +9,17 @@ const images = [
   "images/pic4.jpg"
 ];
 
-
-// btn.forEach(b => b.addEeventListener('click', imgClick));
-
-// function imgClick() {
-
-// }
-
-
+ 
 
 for(let i = 0; i < btn.length; i++){
  
-  btn[i].addEventListener('click', () => {
-    
-    gallery.src = images[i];
-    
-    let current = document.querySelector('.active');
-    current[0].className = current[0].className.replace('active', '');
-    this.className += ' active';
-  });
-
-  
-
-/*
   btn[i].onclick = function() {
     gallery.src = images[i];
-    let current = document.querySelector('.active');
+
+    let current = document.querySelectorAll('.active');
     current[0].className = current[0].className.replace('active', '');
     this.className += ' active';
   }
-*/
+
+ 
 }
